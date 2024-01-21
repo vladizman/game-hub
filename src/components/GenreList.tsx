6f46d70f1ad92982277977551d2c53eaa96a19d2
vladizman <vladizmanq@gmail.com>
2024-01-21 21:00:07 +0200

    selectedgenre bold

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -15,9 +15,10 @@ import {Genre} from "../hooks/useGenres"
 
 interface Props {
   onSelectGenre: (genre: Genre) => void
+  selectedGenre: Genre | null
 }
 
-const GenreList = ({onSelectGenre}: Props) => {
+const GenreList = ({onSelectGenre, selectedGenre}: Props) => {
   const {data, isLoading, error} = useGenres()
 
   if (error) return null
@@ -34,6 +35,7 @@ const GenreList = ({onSelectGenre}: Props) => {
                 src={getCroppedImageUrl(genre.image_background)}
               />
               <Button
+                fontWeight={genre.id === selectedGenre?.id ? "bold" : "normal"}
                 onClick={() => onSelectGenre(genre)}
                 variant="link"
                 fontSize={"lg"}
