8459d3b71c6cee47c753dd8b3e921a391b18d313
vladizman <vladizmanq@gmail.com>
2024-01-23 15:38:07 +0200

    cleanup the genres

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -3,6 +3,7 @@ import useGenres from "../hooks/useGenres"
 import {
   Button,
   HStack,
+  Heading,
   Image,
   List,
   ListItem,
@@ -24,7 +25,10 @@ const GenreList = ({onSelectGenre, selectedGenre}: Props) => {
   if (error) return null
   if (isLoading) return <Spinner></Spinner>
   return (
-    <div>
+    <>
+      <Heading fontSize={"2xl"} marginBottom={3}>
+        Genres
+      </Heading>
       <List>
         {data.map(genre => (
           <ListItem key={genre.id} paddingY={"5px"}>
@@ -32,9 +36,12 @@ const GenreList = ({onSelectGenre, selectedGenre}: Props) => {
               <Image
                 boxSize={"32px"}
                 borderRadius={8}
+                objectFit={"cover"}
                 src={getCroppedImageUrl(genre.image_background)}
               />
               <Button
+                whiteSpace={"normal"}
+                textAlign={"left"}
                 fontWeight={genre.id === selectedGenre?.id ? "bold" : "normal"}
                 onClick={() => onSelectGenre(genre)}
                 variant="link"
@@ -46,7 +53,7 @@ const GenreList = ({onSelectGenre, selectedGenre}: Props) => {
           </ListItem>
         ))}
       </List>
-    </div>
+    </>
   )
 }
 
