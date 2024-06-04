80c463294066401ebf2f4ad4fc0328e66d827234
vladizman <vladizmanq@gmail.com>
2024-06-04 08:40:32 +0300

    add like button

--- src/components/CardGame.tsx
+++ src/components/CardGame.tsx
@@ -1,32 +1,36 @@
-import React from "react"
-import {Game} from "../hooks/useGames"
-import {Card, CardBody, HStack, Heading, Image} from "@chakra-ui/react"
-import CardIconList from "./CardIconList"
-import CriticScore from "./CriticScore"
-import getCroppedImageUrl from "../services/image-crop"
-import Emoji from "./Emoji"
+import React from "react";
+import { Game } from "../hooks/useGames";
+import { Card, CardBody, HStack, Heading, Image } from "@chakra-ui/react";
+import CardIconList from "./CardIconList";
+import CriticScore from "./CriticScore";
+import getCroppedImageUrl from "../services/image-crop";
+import Emoji from "./Emoji";
+import LikeGame from "./LikeGame";
 
 interface Props {
-  game: Game
+  game: Game;
 }
 
-const CardGame = ({game}: Props) => {
+const CardGame = ({ game }: Props) => {
   return (
     <Card>
       <Image src={getCroppedImageUrl(game.background_image)} />
       <CardBody>
         <HStack justifyContent={"space-between"} marginBottom={3}>
           <CardIconList
-            platforms={game.parent_platforms.map(p => p.platform)}
+            platforms={game.parent_platforms.map((p) => p.platform)}
           />
           <CriticScore score={game.metacritic} />
         </HStack>
-        <Heading fontSize="2xl">
-          {game.name} <Emoji rating={game.rating_top} />
-        </Heading>
+        <HStack justifyContent={"space-between"} marginBottom={3}>
+          <Heading fontSize="2xl">
+            {game.name} <Emoji rating={game.rating_top} />{" "}
+            <LikeGame gameId={game.id} />
+          </Heading>
+        </HStack>
       </CardBody>
     </Card>
-  )
-}
+  );
+};
 
-export default CardGame
+export default CardGame;
